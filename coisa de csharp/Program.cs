7d0eac7c85e2d1f95de2a3adedd838203ec06cc1
odanroterif <143523077+odanroterif@users.noreply.github.com>
2023-11-24 17:49:57 -0300

    Add files via upload

---
+++ coisa de csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste_2
+{
+    class Teste__2
+    {
+        static void Main(string[] args)
+        {
+            string color;
+         //---------------------------------
+            Console.Write("digite sua cor favorita: ");
+            color = Console.ReadLine();
+            Console.Clear(); //limpate/system("cls || clear) do C#
+            Console.WriteLine("\n\na cor que você digitou foi " + color);
+            Console.Read();
+        }
+    }
+}
