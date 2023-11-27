79b303355653e795648990fab8890c57addca4f5
odanroterif <143523077+odanroterif@users.noreply.github.com>
2023-11-26 22:16:57 -0300

    Add files via upload

---
+++ coisa de csharp/teste 9.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//-----------------------------------
+namespace teste_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            //try catch: se tiver um erro em vez de fechar o programa você é avisado do ocorrido
+            try
+            {
+                Console.Write("coloque algum número: ");
+                number = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("um erro ocorreu");
+            }
+            Console.ReadLine();
+        }
+    }
+}
