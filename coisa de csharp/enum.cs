d4742917817c6a1903f4cbb58a758f3971feea8e
odanroterif <143523077+odanroterif@users.noreply.github.com>
2023-12-10 11:42:57 -0300

    Add files via upload

---
+++ coisa de csharp/enum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @enum
+{
+    internal class Program
+    {
+        enum MyEnum
+        {
+            coisa,prato,café,armario,calango,pão
+        }
+        static void Main(string[] args)
+        {
+            MyEnum a = MyEnum.armario; //chamada direta
+            MyEnum b = (MyEnum)4;//chamada por número do indice
+            int c = (int)MyEnum.café;//pegando a string do enum e convertendo para seu valor de indice
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
