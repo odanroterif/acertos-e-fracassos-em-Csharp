f056361c629c9d2ca8e4f58be13d15455f135c54
odanroterif <143523077+odanroterif@users.noreply.github.com>
2023-12-10 12:41:27 -0300

    Add files via upload

---
+++ coisa de csharp/ale.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[3];
+            Random b = new Random();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = b.Next(100);
+            }
+            Console.Clear();
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[1]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
