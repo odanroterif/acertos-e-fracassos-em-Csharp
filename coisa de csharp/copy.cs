32225d7fffc22ef94f2ba8b3e4e3e8e13d474fbc
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-01-04 16:36:17 -0300

    Add files via upload

---
+++ coisa de csharp/copy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace copy
+{
+    internal class Copy
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[3];
+            int[] b = new int[3];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write("digite um número: ");
+                a[i] = int.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+            //a.CopyTo(b,1); <-- caso o vetor b fosse maior
+            Array.Copy(a, b, a.Length);
+            foreach (int item in b)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
