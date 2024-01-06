0ec79beb5c65d8870b258f1a2908b473e3896f2d
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-01-06 16:34:50 -0300

    Add files via upload

---
+++ coisa de csharp/ref.cs
@@ -0,0 +1,31 @@
+﻿using binaryseach;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passagem_ref
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Saida exit = new Saida();   
+
+            Console.Write("digite qualquer número: ");
+            number = int.Parse(Console.ReadLine()); 
+            //ref = variavel referêncial, em vez de criar outro espaço na memoria altera o espaço
+            //que ja existe
+            valuer(ref number); 
+            Console.Clear();
+            Console.WriteLine("valor do número digitado dobrado para "+number);
+            exit.exit();
+        }
+        static void valuer(ref int a)
+        {
+            a *= 2;
+        }
+    }
+}
