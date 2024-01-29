918fc46e67b1b80d7bd6d378facab6afc9f6cbb3
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-01-29 20:29:49 -0300

    Add files via upload

---
+++ coisa de csharp/Saida.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binaryseach
+{
+    public class Saida
+    {
+        public void exit()
+        {
+            Console.WriteLine("\n\n\n\npressione qualquer tecla para encerrar o programa....");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ coisa de csharp/sobrecargadeconstrutores.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using binaryseach;
+
+namespace sobrecargadeconstrutores
+{
+    internal class player
+    {
+        int life;
+        float attack;
+        bool takedamage;
+        //sobrecarga de construtores: vários construtores na mesma classe
+        public player(int a)
+        {
+            life = a;
+            attack = 4.7f;
+            takedamage = true;
+        }
+        public player(float b)
+        {
+            life = 100;
+            attack = b;
+            takedamage = true;
+        }
+        public player(int a, float b)
+        {
+            life = a;
+            attack = b;
+            takedamage = true;
+        }
+        public player(int a, float b, bool c)
+        {
+            life = a;
+            attack = b;
+            takedamage = c;
+        }
+        public void stats()
+        {
+            Console.WriteLine("health: " + life);
+            Console.WriteLine("attack: " + attack);
+            Console.WriteLine("dano no processo ?: " + takedamage + "\n\n");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saida e = new Saida();
+            player player_one = new player(110, 5.2f);
+            player player_two = new player(40, 9.3f, false);
+
+            Console.WriteLine("ESTATISTICAS DE PLAYERS\n\n");
+            player_one.stats();
+            player_two.stats();
+            e.exit();
+        }
+    }
+}
