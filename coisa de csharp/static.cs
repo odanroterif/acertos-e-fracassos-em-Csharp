1e2c9f68f1fee85ce913636e03ac6a94afbf66a5
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-02-02 20:48:33 -0300

    Add files via upload

---
+++ coisa de csharp/static.cs
@@ -0,0 +1,48 @@
+﻿using binaryseach;
+using System;
+
+namespace @static
+{
+    static public class sans
+    {
+        static public bool rage;
+        static public int life;
+
+        static sans()
+        {
+            rage = false;
+            life = 5;
+        }
+        static public void bone(bool a, int b)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+        static public void bone(int b)
+        {
+            Console.WriteLine(rage);
+            Console.WriteLine(b);
+        }
+        static public void bone(bool a)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(life);
+        }
+        static public void bone()
+        {
+            Console.WriteLine(rage);
+            Console.WriteLine(life);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saida e = new Saida();
+
+            sans.rage = true;
+            sans.bone();
+            e.exit();
+        }
+    }
+}
---
+++ coisa de csharp/this.cs
@@ -0,0 +1,32 @@
+﻿using binaryseach;
+using System;
+
+namespace @this
+{
+    public class basic
+    {
+        public int a;
+        public int b;
+
+        public basic(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int math()
+        {
+            return a + b;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saida e = new Saida();
+            basic calculator = new basic(5, 9);
+
+            Console.WriteLine("\n\n" + calculator.math());
+            e.exit();
+        }
+    }
+}
