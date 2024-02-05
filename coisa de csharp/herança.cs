937ebd584a9d5f3029089160cff3b6b2abf75025
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-02-05 15:19:06 -0300

    Add files via upload

---
+++ coisa de csharp/herança.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herança
+{
+    public class status
+    {
+        private bool carstatus;
+
+        public bool on()
+        {
+            carstatus = true;
+
+            return carstatus;
+        }
+        public bool off()
+        {
+            carstatus = false;
+
+            return carstatus;
+        }
+    }
+    public class car : status
+    {
+        public void rotation()
+        {
+
+        }    
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car CAR = new car();
+
+            Console.WriteLine(CAR.off());
+            Console.ReadLine();
+        }
+    }
+}
