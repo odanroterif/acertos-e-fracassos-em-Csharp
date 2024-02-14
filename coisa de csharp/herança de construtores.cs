c040b4b54c46022a61a0b0d0b9ae5c8d88af2c8f
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-02-14 14:52:30 -0300

    Add files via upload

---
+++ coisa de csharp/herança de construtores.cs
@@ -0,0 +1,60 @@
+﻿using binaryseach;
+using System;
+
+namespace construtor_na_classe_pai
+{
+    public class banban
+    {
+        private float garp;
+
+        public banban()
+        {
+        }
+        public banban(float garp)
+        {
+            this.garp = garp * garp;
+            if (this.garp > 8000.0f)
+            {
+                this.garp = 8000f;
+            }
+        }
+        public void final()
+        {
+            Console.WriteLine(garp);
+        }
+    }
+    public class garten : banban
+    {
+        public garten(int a)
+        {
+            Console.WriteLine(a);
+        }
+        public garten(float s) : base(s)
+        {
+            final();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saida e = new Saida();
+            float noidea;
+
+        Start:
+            Console.Write("Digite: ");
+            noidea = float.Parse(Console.ReadLine());
+            Console.Clear();
+            if (noidea > 100.0f)
+            {
+                goto Start;
+            }
+
+            garten of = new garten((int)noidea);
+            garten five = new garten(noidea);
+
+
+            e.exit();
+        }
+    }
+}
