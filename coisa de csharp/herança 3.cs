b0ce4a4d70be4941703394015c9d7940fa1987bb
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-02-18 11:30:26 -0300

    Add files via upload

---
+++ coisa de csharp/herança 3.cs
@@ -0,0 +1,41 @@
+﻿using binaryseach;
+using System;
+
+namespace herança_3
+{
+    public class primeiro
+    {
+        public int number = 1;
+
+        public primeiro()
+        {
+            Console.WriteLine("\n\n" + number);
+        }
+    }
+    public class segundo : primeiro
+    {
+        public segundo()
+        {
+            number = 2;
+            Console.WriteLine(number);
+        }
+    }
+    public class terceiro : segundo
+    {
+        public terceiro()
+        {
+            number = 3;
+            Console.WriteLine(number);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            terceiro sequence = new terceiro();
+            Saida e = new Saida();
+
+            e.exit();
+        }
+    }
+}
