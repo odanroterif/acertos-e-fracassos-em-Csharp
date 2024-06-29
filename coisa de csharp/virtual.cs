46af17e67fc7680e99ac138a69144e09cc7db84f
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-06-29 14:05:35 -0300

    Add files via upload

---
+++ coisa de csharp/virtual.cs
@@ -0,0 +1,41 @@
+﻿using binaryseach;
+using System;
+
+
+namespace @virtual
+{
+    public class life_solution
+    {
+        public virtual int life()
+        {
+            return 0;
+        }
+    }
+    public class trader : life_solution
+    {
+        private int health = 14;
+        private string Name = "vendedor ambulante";
+
+        public string name()
+        {
+            return Name;
+        }
+        public override int life()
+        {
+            return health;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saida e = new Saida();
+            trader trader = new trader();
+
+            Console.WriteLine("criatura: " + trader.name());
+            Console.WriteLine("vida: " + trader.life());           
+            e.exit();
+        }
+    }
+}
