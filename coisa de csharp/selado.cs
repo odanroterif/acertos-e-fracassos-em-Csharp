38bd6ad68bfc53bd573fca63963dbedc795eb3cc
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-07-27 16:24:14 -0300

    Add files via upload

---
+++ coisa de csharp/selado.cs
@@ -0,0 +1,26 @@
+﻿using binaryseach;
+using System;
+
+namespace @sealed
+{
+    sealed class player //: piglin <--- player pode herdar piglin, mas piglin não pode herdar player
+    {
+        int life = 20;
+        string name = "steve";
+        float damage = 0.5f;
+    }
+    public class piglin //: player <--- nada pode inteferir em player a não ser que seja direto na classe dele
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saida e = new Saida();
+
+            Console.WriteLine("é só isso? -_-");
+            e.exit();
+        }
+    }
+}
