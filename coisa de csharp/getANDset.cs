4b16809564c5c81916d225853c92387dc3feab87
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-08-14 10:29:14 -0300

    Add files via upload

---
+++ coisa de csharp/getANDset.cs
@@ -0,0 +1,48 @@
+﻿using binaryseach;
+using System;
+
+namespace getANDset
+{
+    public class mob_sound
+    {
+        private string sound;
+
+        public string mob_name
+        {
+            get { return sound; }
+            set
+            {
+                switch (value)
+                {
+                    case "creeper":
+                        sound = "tsss...";
+                        break;
+                    case "enderman":
+                        sound = "hello";
+                        break;
+                    default:
+                        sound = "......";
+                        break;
+                }
+            }
+
+        }
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Saida e = new Saida();
+            mob_sound search = new mob_sound();
+
+            Console.Write("nome do mob: ");
+            string resward = Console.ReadLine().ToLower();
+            search.mob_name = resward;
+            Console.Clear();
+            Console.WriteLine(search.mob_name);
+            e.exit();
+        }
+    }
+}
