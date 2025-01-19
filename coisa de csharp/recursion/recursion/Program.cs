1222fe567bc89ca50774d920fbf37ab3e183d5fe
odanroterif <143523077+odanroterif@users.noreply.github.com>
2025-01-19 19:22:06 -0300

    Add files via upload

---
+++ coisa de csharp/recursion/recursion/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace recursion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o valor: ");
+        var val = ulong.Parse(Console.ReadLine()!);
+        Console.Clear();
+        Console.Write($"o fatorial de {val} é {Factorial(val)}");
+        //Factorial("oi"); segundo exemplo de recursividade
+    }
+/*
+    static void Factorial(string thing)
+    {
+        Console.Clear();
+        Console.Write($"digite algo diferente de {thing}: ");
+        var fac = Console.ReadLine();
+        if (fac == "paralelepipedo")
+        {
+            Console.WriteLine("passou");
+        }
+        else
+        {
+            Factorial(fac);
+        }
+    }
+*/
+    static ulong Factorial(ulong number)
+    {
+        ulong fac;
+
+        if (number < 1)
+        {
+            fac = 1;
+        }
+        else
+        {
+            fac = number * Factorial(number - 1);
+        }
+
+        return fac;
+    }
+}
