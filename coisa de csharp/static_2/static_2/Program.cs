fbe90048dbe023fa1e63091a7b439c7746bd8885
odanroterif <143523077+odanroterif@users.noreply.github.com>
2025-01-22 12:25:57 -0300

    Add files via upload

---
+++ coisa de csharp/static_2/static_2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace static_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um valor: ");
+        Upnumber.number = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+        Console.WriteLine("quer o dobro ou o triplo deste número?");
+        var aswer = Console.ReadLine();
+        Console.Clear();
+        
+        if(aswer!.ToLower() == "dobro")
+            Console.WriteLine($"o dobro do {Upnumber.number} é {Upnumber.Duo(Upnumber.number)}");
+        else if(aswer.ToLower() == "triplo")
+            Console.WriteLine($"o triplo de {Upnumber.number} é {Upnumber.Tri(Upnumber.number)}");
+        else
+        {
+            Console.WriteLine("não temos esta operação");
+        }
+    }
+}
---
+++ coisa de csharp/static_2/static_2/Upnumber.cs
@@ -0,0 +1,15 @@
+namespace static_2;
+
+public class Upnumber
+{
+    public static int number;
+    public static int Duo(int number)
+    {
+        return number * 2;
+    }
+
+    public static int Tri(int number)
+    {
+        return number * 3;
+    }
+}
