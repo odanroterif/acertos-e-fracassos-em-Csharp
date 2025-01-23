10cfa4f45352ecb751964cda46921941a9c97c6b
odanroterif <143523077+odanroterif@users.noreply.github.com>
2025-01-23 11:53:36 -0300

    Add files via upload

---
+++ coisa de csharp/_delegates/_delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _delegates;
+
+delegate float GeneralF(params float[] number);
+delegate int General(params int[] number);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GeneralF ranky = BasicMath.Sum;
+        General franky = BasicMath.Sum;
+        
+        var mat = ranky(45,23.2f);
+        var mat2 = franky(45,11);
+        
+        franky = BasicMath.Multiplication;
+        ranky = BasicMath.Multiplication;
+        
+        var mat3 = franky(23,70);
+        var mat4 = ranky(12.45f,36.1f);
+        
+        Console.WriteLine($"{mat:N2}\n{mat2}\n{mat3}\n{mat4:N2}");
+    }
+}
