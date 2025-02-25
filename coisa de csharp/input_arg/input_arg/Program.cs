c9a5956a543c57430a0d8fd6feb53bb9446b2b1c
odanroterif <143523077+odanroterif@users.noreply.github.com>
2025-02-25 09:33:04 -0300

    Add files via upload

---
+++ coisa de csharp/input_arg/input_arg/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace input_arg;
+
+//argumentos de entrada para o programa
+class Program
+{
+    static void Main(string[] args)//no caso de aplicativo de console o argumento sera passado via terminal
+    {
+        if (args.Length > 0)
+            Console.WriteLine($"{args.Length} argumentos encontrados no programa.");
+        else
+        {
+            Console.WriteLine("Este programa não recebeu argumentos e ira operar de forma padrão");
+        }
+    }
+}
