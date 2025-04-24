e19194ca87e3335e4136f31bb68936be0a45940e
odanroterif <143523077+odanroterif@users.noreply.github.com>
2025-04-23 21:45:20 -0300

    Update PController.cs

--- backend3/Controllers/PController.cs
+++ backend3/Controllers/PController.cs
@@ -1,6 +1,7 @@
 using backend3.data;
 using backend3.models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend3.Controllers;
 
@@ -23,10 +24,55 @@ public class PController : ControllerBase
         
         return Ok(person);
     }
-    /*
+    
     [HttpGet]
-    public async Task<ActionResult<Person>> Viewer()
+    public async Task<ActionResult<IEnumerable<Person>>> ViewerAll()
     {
-        return Ok(_context.SimpleCrud.ToList());
-    }*/
+        var personList = await _context.SimpleCrud.ToListAsync();
+        
+        return Ok(personList);
+    }
+    
+    [HttpGet("ordemalfabeto")]
+    public async Task<ActionResult<IEnumerable<Person>>> AlphabetOrder()
+    {
+        var personList = await _context.SimpleCrud.OrderBy(p => p.Name).ToListAsync();
+        
+        return Ok(personList);
+    }
+    
+    [HttpGet("gentesemsenha")]
+    public async Task<ActionResult<IEnumerable<Person>>> Viewer()
+    {
+        var personList = await _context.SimpleCrud.Where(p => p.Password != "").ToListAsync();
+        
+        return Ok(personList);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Person>> Update(int id, Person person)
+    {
+        if (id != person.Id)
+        {
+            return BadRequest("A atualização falhou, a pessoa que você busca não existe");
+        }
+        
+        _context.Entry(person).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        
+        return Ok(person);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Person>> Del(int id, Person person)
+    {
+        if (id != person.Id)
+        {
+            return BadRequest("Aviso: a pessoa que você procura ja foi removida ou não existe");
+        }
+        _context.SimpleCrud.Remove(person);
+        await _context.SaveChangesAsync();
+        
+        return Ok(person);
+    }
 }
