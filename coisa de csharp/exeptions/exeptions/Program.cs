7f33ae03240f960888cdf97b0e0069b369ba5c07
odanroterif <143523077+odanroterif@users.noreply.github.com>
2025-06-04 19:23:51 -0300

    Add files via upload

---
+++ coisa de csharp/exeptions/exeptions/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+namespace exeptions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var number = new int[2];
+      
+        for (var c = 0; c < number.Length; c++)
+        {
+            try
+            {
+                Write($"{c + 1}॰ número: ");
+                number[c] = int.Parse(ReadLine() ?? string.Empty); 
+                Clear();
+            }
+            catch (Exception e)
+            {
+                Clear();
+                WriteLine($"um erro aconteceu: {e.GetType().Name}, {e.Message} \ninsira um número por favor");
+                c--;
+            }
+        }
+
+        WriteLine(number[0] + number[1]);
+        ReadKey();
+    }
+}
