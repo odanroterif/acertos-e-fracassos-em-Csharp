25fcff6d58e62be1860d2cf146fbce6d1b82476e
odanroterif <143523077+odanroterif@users.noreply.github.com>
2025-06-16 09:21:55 -0300

    Add files via upload

---
+++ coisa de csharp/_namespace/_namespace/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+namespace _namespace
+{
+    class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, World!");
+            WriteLine(_another_namespace.Program.Foo());
+        }
+    }
+}
+
+// é possivel reutilizar os mesmos nomes de classes variaveis e funções se eles estiverem em outro namespace
+namespace _another_namespace
+{
+    class Program
+    {
+        // ReSharper disable once UnusedMember.Local
+        public static string Foo() => "outro Program de outro namespace";
+    }
+}
---
+++ coisa de csharp/exceptions/exceptions/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+
+namespace exceptions;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var password = new[] {"adamatium123", " "};
+
+        for (Int16 c = 0; c <= 3; c++)
+        {
+            try
+            {
+                Write($"Digite a senha correta: ");
+                password[1] = ReadLine() ?? string.Empty; 
+                Clear();
+                
+                if (password[1] != password[0])
+                    throw new Exception($"senha incorreta, tentativa {c + 1}");
+                else
+                {
+                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                Clear();
+                WriteLine(e.Message);
+            }
+            finally
+            {
+                if (c == 3 && password[1] != password[0])
+                {
+                    Clear();
+                    WriteLine("acesso negado");
+                }
+            }
+        }
+        
+        if(password[1] == password[0])
+            WriteLine("acesso permitido");
+        
+        
+        ReadKey();
+    }
+}
